988e9de4bebe2b311151c931bef610ff46525dc7
cfarot <charlyfarot@gmail.com>
2015-05-18 15:35:39 +0200

    Request modif

--- src/serveur/Request.java
+++ src/serveur/Request.java
@@ -1,8 +1,12 @@
 package serveur;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,25 +14,41 @@ import java.util.Map;
 public class Request {
 
 	private Map<String, String> dataHeader = new HashMap<String, String>();
+	private Socket mySocket;
 	
 	public Request(Socket socket){
 		
+		mySocket = socket;
 		BufferedReader in;
 		try {
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
 			String MethodLine = in.readLine();
 			dataHeader.put("METHOD", MethodLine.split(" ")[0]);
 			dataHeader.put("Ressource", MethodLine.split(" ")[1]);
 
 			String HostLine = in.readLine();
-			//System.out.println(HostLine);
+			dataHeader.put("Host", HostLine.split(" ")[1]);
 			in.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	private String getRelativeUrl(){
+		return getHeader("Ressource");
+	}
+	
+	private String getHost(){
+		return getHeader("Host");
+	}
+	
 	private String getHeader(String header){
 		return dataHeader.get(header);
 	}
+	
+	private Writer getWritter() throws IOException{
+		return new PrintWriter(new BufferedWriter(new OutputStreamWriter(mySocket.getOutputStream())));
+	}
+	
+	
 }
